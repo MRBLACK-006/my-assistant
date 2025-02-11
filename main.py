3e1f227a5f2dee875544d04de0260681eeedc285
MRBLACK-006 <ashwanthg006@gmail.com>
2025-02-11 12:38:33 +0530

    commit

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
-
 import speech_recognition as sr
-import pyttsx3 
+import pyttsx3
 import webbrowser
 import subprocess
 import pyautogui
 import time
 import datetime
-# Initialize the recognizer 
-r = sr.Recognizer() 
 
-# Function to convert text to
-# speech
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Function to convert text to speech
 def SpeakText(command):
-    
-    # Initialize the engine
     engine = pyttsx3.init()
-    engine.say(command) 
+    engine.say(command)
     engine.runAndWait()
 
-
+# Function to open Chrome with a given URL
 def open_chrome(url):
-   browser = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-   webbrowser.get(browser)
-   #You can do webbrowser.open(url, 0) if you want to open in the same window, 1 is a new window, 2 is a new tab. Default behaviour opens them in a new tab anyway.
-   #See https://docs.python.org/2/library/webbrowser.html
-   webbrowser.open(url)
-    
+    browser = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(browser)
+    webbrowser.open(url)
+
+# Function to open Spotify and play/pause music
 def Spotify():
-    subprocess.Popen([r"C:\\Users\\MR.BLACK\AppData\\Roaming\Spotify\Spotify.exe"])
+    subprocess.Popen([r"C:\\Users\\MR.BLACK\\AppData\\Roaming\\Spotify\\Spotify.exe"])
     time.sleep(5)
     pyautogui.press("playpause")
     SpeakText("playing")
 
-    
-# Loop infinitely for user to
-# speak
+# Function to open a game
+def game():
+    exe_path = r'"D:\GAMES\Horizon - Zero Down CE\HorizonZeroDawn.exe"'
+    subprocess.run(["powershell", "Start-Process", exe_path, "-Verb", "RunAs"], shell=True)
+
+# Function to check if a command is in a list of commands
+def cmdChecker(mytext, cmdlist):
+    for i in cmdlist:
+        if mytext == i:
+            return True
+    return False
+
+# Function to tell the current time
+def tell_time():
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M")
+    SpeakText(f"The current time is {current_time}")
+
+# Greet the user based on the time of day
 hour = datetime.datetime.now().hour
 greetings = ""
 if 5 <= hour < 12:
@@ -42,63 +54,64 @@ if 5 <= hour < 12:
 elif 12 <= hour < 17:
     greetings = "Good Afternoon"
 elif 17 <= hour < 21:
-    greetings = "Good Evneing"
+    greetings = "Good Evening"
 else:
     greetings = "Good Night"
 
-SpeakText(greetings + " sir i am here to serve you")
-while(1):    
-    print("speak : ")
-    
-    # Exception handling to handle
-    # exceptions at the runtime
+SpeakText(greetings + " .......I am your AI assistant, ...I am here to assist you")
+
+# Loop infinitely for user to speak
+while True:
+    print("Speak: ")
+
     try:
-        
-        # use the microphone as source for input.
         with sr.Microphone() as source2:
-            
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level 
             r.adjust_for_ambient_noise(source2, duration=0.2)
-            
-            #listens for the user's input 
             audio2 = r.listen(source2)
-            
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio2)
-            # MyText = r.recognize_tensorflow(audio2)
+            MyText = r.recognize_google(audio2).lower()
+            print("You said: ", MyText)
 
-            MyText = MyText.lower()
-            print("you said : ",MyText)
-
-            if(MyText == "open chrome"):
+            if MyText == "open chrome":
                 SpeakText("opening chrome")
                 open_chrome("www.google.com")
-            elif(MyText == "open youtube"):
+            elif MyText == "open youtube":
                 SpeakText("opening youtube")
                 open_chrome("www.youtube.com")
-            elif(MyText== "open spotify" or MyText == "play some song"or MyText== "play some music"):
-                SpeakText("okay dumb shit am opening spotify" )
+            elif cmdChecker(MyText, ["open spotify", "play some song", "play some music"]):
+                SpeakText("okay, opening spotify")
                 Spotify()
-            elif(MyText == "volume up"):
+            elif MyText == "volume up":
                 pyautogui.press("volumeup")
-            elif(MyText == "volume down"):
+            elif MyText == "volume down":
                 pyautogui.press("volumedown")
-            elif("search" in MyText):
-                "search youtube"
+            elif "search" in MyText:
                 search_text = MyText[7:]
-                open_chrome("https://www.google.com/search?q="+search_text)
+                open_chrome("https://www.google.com/search?q=" + search_text)
                 SpeakText("searching " + search_text)
-            elif(MyText == "goodbye"):
-                SpeakText("bye bye see you later ")
+            elif cmdChecker(MyText, ["open horizon", "open horizon zero dawn"]):
+                SpeakText("okay, opening horizon zero dawn")
+                game()
+            elif MyText == "what time is it":
+                tell_time()
+            elif MyText == "open notepad":
+                SpeakText("opening notepad")
+                subprocess.Popen(["notepad.exe"])
+            elif MyText == "open calculator":
+                SpeakText("opening calculator")
+                subprocess.Popen(["calc.exe"])
+            elif MyText == "take screenshot":
+                SpeakText("taking screenshot")
+                screenshot = pyautogui.screenshot()
+                screenshot.save("screenshot.png")
+                SpeakText("screenshot saved")
+            elif MyText == "goodbye":
+                SpeakText("bye bye....see you later")
                 break
+            else:
+                SpeakText("UNKNOWN COMMAND!")
+                print("UNKNOWN COMMAND!")
 
-            
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
-        
     except sr.UnknownValueError:
         print("unknown error occurred")
-
-
